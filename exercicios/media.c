3abf70e9857824d0053b4a26ea10d33aad4b3549
GilbertoAlves <143003378+gilbertoalvs@users.noreply.github.com>
2026-03-12 19:40:08 +0000

    Implementa validação na entrada e muda variáveis para double.

--- exercicios/media.c
+++ exercicios/media.c
@@ -10,10 +10,27 @@ int main(){
 
     for(int i =0; i < TOTAL_ALUNOS; i++){
         printf("Digite as notas do aluno %d:\n", i + 1);
-        printf("Nota 1: ");
-        scanf("%lf", &nota1[i]);
-        printf("Nota 2: ");
-        scanf("%lf", &nota2[i]);
+        
+        while (1) {
+            printf("Nota 1 (0 a 10): ");
+        
+            if (scanf("%lf", &nota1[i]) == 1 && nota1[i] >= 0 && nota1[i] <= 10) {
+                break; // Entrada válida, sai do loop
+            } else {
+                printf("Erro: Digite um numero valido entre 0 e 10.\n");
+                while (getchar() != '\n'); 
+            }
+        }
+
+        while (1) {
+            printf("Nota 2 (0 a 10): ");
+            if (scanf("%lf", &nota2[i]) == 1 && nota2[i] >= 0 && nota2[i] <= 10) {
+                break; 
+            } else {
+                printf("Erro: Digite um numero valido entre 0 e 10.\n");
+                while (getchar() != '\n'); 
+            }
+        }
 
         media[i] = (nota1[i] * 2 + nota2[i] * 3) / 5;
 
@@ -22,15 +39,15 @@ int main(){
 
     media_turma = soma / TOTAL_ALUNOS;
 
-    printf("--- RESULTADOS ---\n");
-    printf("Media da turma: %.2f\n", media_turma);
-    printf("------------------\n");
+    printf("\n============================\n");
+    printf("Media da Turma: %.2f\n", media_turma);
+    printf("============================\n");
 
-    printf("Alunos com media superior a media da turma:\n");
-
-    for(int i = 0; i < TOTAL_ALUNOS; i++){
-        if (media[i] > media_turma){
-            printf("Aluno %d = Nota 1: %.1f | Nota 2: %.1f | Media: %.2f\n", i + 1, nota1[i], nota2[i], media[i]);
+    printf("Alunos acima da media:\n");
+    for (int i = 0; i < TOTAL_ALUNOS; i++) {
+        if (media[i] > media_turma) {
+            printf("Aluno %d -> N1: %.1f | N2: %.1f | Media: %.2f\n", 
+                    i + 1, nota1[i], nota2[i], media[i]);
         }
     }
 
